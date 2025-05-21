f3914dd16726096f8119b03ebf59abfe4d0a0d88
clarapereiraa <clasipereira@gmail.com>
2025-05-21 15:05:19 -0300

    mudando ip e add tiggers e docker

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     connectionLimit:10,
-    host:'10.89.240.99',
+    host:'10.89.240.66',
     user:'alunods',
     password:'senai@604',
     database:'banco_salas'
